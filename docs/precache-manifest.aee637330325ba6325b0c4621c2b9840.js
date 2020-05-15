5eae4360187e5d253f629f527fe0333228397ee2
Grisel Matos <griselmatosm@gmail.com>
2020-05-15 14:39:24 +0200

    Push docs folder

---
+++ docs/precache-manifest.aee637330325ba6325b0c4621c2b9840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808052027ec08c0fb25ab83a8e1eaa07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "866e8d060778a0048519",
+    "url": "./static/css/main.6caa5ee1.chunk.css"
+  },
+  {
+    "revision": "53722318f2a52f6af820",
+    "url": "./static/js/2.24fb346a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.24fb346a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866e8d060778a0048519",
+    "url": "./static/js/main.259382ac.chunk.js"
+  },
+  {
+    "revision": "6a5ee69cb98cb1f73811",
+    "url": "./static/js/runtime-main.cb72faa7.js"
+  }
+]);
